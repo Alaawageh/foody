8fc2b939971301d8d59b89096df8143762d81961
Alaa wageh <alaawageh199@gmail.com>
2023-08-20 08:53:38 +0300

    update file

--- app/Http/Resources/ResturantResource.php
+++ app/Http/Resources/ResturantResource.php
@@ -14,6 +14,13 @@ class ResturantResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // 
+        return [
+            'id'=>$this->id,
+            'resturant name'=>$this->resturant_name,
+            'email'=>$this->email,
+            'phone' => $this->phone,
+            'address'=>$this->address
+        ];
     }
 }
