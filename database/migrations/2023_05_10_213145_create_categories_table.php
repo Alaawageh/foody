3b2472b223081cff8705d2ee308c193367c79b87
Alaa wageh <alaawageh199@gmail.com>
2023-08-20 08:41:38 +0300

    update file

--- database/migrations/2023_05_10_213145_create_categories_table.php
+++ database/migrations/2023_05_10_213145_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('position')->nullable();
+            $table->bigInteger('position')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
